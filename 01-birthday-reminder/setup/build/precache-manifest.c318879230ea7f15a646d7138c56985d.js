8e5639f39b664dd7a4f270b5d5e1b60ee25ec9b0
arpitpatawat <arpitpatawat2000@gmail.com>
2022-06-05 18:54:03 +0530

    added node modules

---
+++ 01-birthday-reminder/setup/build/precache-manifest.c318879230ea7f15a646d7138c56985d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e16f9f3f5e610d62294f9ad0cd5ce5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43726a369a5993e57e34",
+    "url": "/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "8a695afdbb9a20593d81",
+    "url": "/static/js/2.3915628d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3915628d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43726a369a5993e57e34",
+    "url": "/static/js/main.22f530c8.chunk.js"
+  },
+  {
+    "revision": "49de1d56170d62cf3751",
+    "url": "/static/js/runtime-main.d0798475.js"
+  }
+]);
