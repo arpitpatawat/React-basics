1fc0e906e869f78764bba9aef0cfc20ce2003ac2
arpitpatawat <arpitpatawat2000@gmail.com>
2022-06-10 16:15:05 +0530

    added tabs

---
+++ 06-tabs/setup/build/precache-manifest.0030cbe26d158a8f7309658e198bcb44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00dbf40fc98e688d2db2eaf42d98b706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af1099237b327df0f665",
+    "url": "/static/css/main.77ad5ced.chunk.css"
+  },
+  {
+    "revision": "f150b89cb881ab50f97a",
+    "url": "/static/js/2.b177759b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b177759b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1099237b327df0f665",
+    "url": "/static/js/main.fef378e5.chunk.js"
+  },
+  {
+    "revision": "49de1d56170d62cf3751",
+    "url": "/static/js/runtime-main.d0798475.js"
+  }
+]);
